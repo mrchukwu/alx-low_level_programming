e697c8e4fd9d7842bb21ff6918636ff39951e951
mrchukwu <mrchukwusinbox@gmail.com>
2022-10-13 17:16:20 -0700

    write a c program that prints exactly Programming is a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Main servers as the entry point into the program
+ *
+ * Return: by default returns zero, meaning the code exe. Otherwise one meaning it didnt exe
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
