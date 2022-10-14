46819d5ad26efec4a232611131db3be408f94f4c
mrchukwu <mrchukwusinbox@gmail.com>
2022-10-13 17:29:45 -0700

    write a script that prints an output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program
+ *
+ * return: zero if no erros, return non-zero if errors
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
