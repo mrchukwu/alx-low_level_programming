f2714fc7e4e8e02d376785f0bbc9bc30759081bd
mrchukwu <mrchukwusinbox@gmail.com>
2022-10-15 16:53:10 -0700

    write a program that will print alphabets a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-program entry point.
+ * Return:0 no error,non zero value if error.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
