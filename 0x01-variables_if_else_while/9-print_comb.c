ca6d9459170e5046f95e8c8e167b4881e646a5e8
mrchukwu <mrchukwusinbox@gmail.com>
2022-10-15 17:43:09 -0700

    write a program that prints all possible signle digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+i#include <stdio.h>
+/**
+ * main-program entry point.
+ * Return:0 no error,non zero value if error.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num >= '9'; num++)
+	{
+		putchar(num);
+		if (num == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
