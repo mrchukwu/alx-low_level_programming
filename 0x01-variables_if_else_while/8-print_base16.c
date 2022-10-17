ae74490d0cb1982db56a167a8ac3dbd42217debb
mrchukwu <mrchukwusinbox@gmail.com>
2022-10-17 14:43:36 -0700

    update8-print_base16.c file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main-program entry point.
+ * main- program entry point.
  * Return:0 no error,non zero value if error.
  */
 int main(void)
@@ -8,7 +8,7 @@ int main(void)
 	int i;
 	char hexvalues[] = "0123456789abcdef";
 
-	for (i = 0; i <= 16; i++)
+	for (i = 0; i < 16; i++)
 	{
 		putchar(hexvalues[i]);
 	}
