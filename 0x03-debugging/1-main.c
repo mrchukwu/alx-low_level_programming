045fe68ae3054daea2c9ba6c3498d53574ed7b13
mrchukwu <mrchukwusinbox@gmail.com>
2022-10-20 08:23:38 -0700

    main.c file commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- causes an infinite loop
+ * Return:0 success, non-zero digit if error
+ */
+
+int main(void)
+{
+	/* int i; */
+
+	printf("infinite loop incoming : (\n");
+
+	/*
+	 * i = 0;
+	 *
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 *}
+	 */
+
+	 printf("infinte loop avoided! \\0/\n");
+
+	 return (0);
+}
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+i
+/**
+ * main- tests function that prints if integer is positive or negative
+ * Return:0 success, non-zero digit if error
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
