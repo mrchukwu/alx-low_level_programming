fc2517ede1a433a869add048ee0020b96075f00e
mrchukwu <mrchukwusinbox@gmail.com>
2022-10-24 07:26:14 -0700

    update on the 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ii#include "main.h"
+
+/**
+ * swap_int- swaps the value of two integers
+ * @a: value to be evaluated
+ * @b: value to be evaluated
+ * Return: not
+ */
+
+voiid swap_int(int *a, int *b)
+{
+	int n = *a;
+
+	*a = *b;
+	*b = n;
+}
