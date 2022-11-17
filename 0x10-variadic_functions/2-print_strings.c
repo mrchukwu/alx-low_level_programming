e09c572ef15e72ef42e4067ba0fb6cfc748313ec
mrchukwu <mrchukwusinbox@gmail.com>
2022-11-18 00:10:35 +0100

    update commit 2-print_strings.c file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,37 +1,31 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
+
 /**
- * print_strings - Entry Point
- * @separator: comma space
- * @n: number of elements
- * Return: void
+ * print_strings - Prints strings passed to the function separed by separators
+ * @separator: A string that is used to separate strings
+ * @n: number of unnamed parameters
+ * Return: Always 0
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
 	char *str;
+	unsigned int i;
+	va_list ap;
 
+	va_start(ap, n);
 	if (separator == NULL)
-		return;
-
-	va_start(valist, n);
+		separator = "";
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(valist, char *);
+		str = va_arg(ap, char*);
 		if (str == NULL)
-		{
 			str = "(nil)";
-		}
 		printf("%s", str);
-		if (i != (n - 1))
-		{
+		if (i < n - 1)
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
-
-	va_end(valist);
+	va_end(ap);
 }
